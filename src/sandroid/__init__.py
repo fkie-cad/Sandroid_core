12be966fe019f0858bab30b4ad5244b495536c82
Daniel Baier <admin@remoteshell-security.com>
2025-08-25 10:43:05 +0200

    update publishing workflow

--- src/sandroid/__init__.py
+++ src/sandroid/__init__.py
@@ -1,6 +1,6 @@
 """Sandroid: An Android sandbox for automated Forensic, Malware, and Security Analysis."""
 
-from ._about import __author__, __authors__, __description__, __email__, __version__
+from .__about import __author__, __authors__, __description__, __email__, __version__
 from .config.loader import ConfigLoader
 from .config.schema import SandroidConfig
 
